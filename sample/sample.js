aacb49edcec41279385df42a5b58e338b70705ef
y-code <yasu.gemini@gmail.com>
2019-05-17 18:48:52 +1200

    Update sample.js

--- sample/sample.js
+++ sample/sample.js
@@ -79,56 +79,7 @@ class Sample2 extends React.Component {
             <h2>Sample 2</h2>
           </Col>
         </Row>
-        <Row>
-          <Col sm='4'>
-            <Row>
-              <Col className='d-flex justify-content-center'>
-                Items in Page
-              </Col>
-            </Row>
-            <Row>
-              <Col className='d-flex justify-content-center'>
-                <ButtonGroup>
-                  <Button onClick={() => this.setState({pageSize: 3})} active={this.state.pageSize === 3}>3</Button>
-                  <Button onClick={() => this.setState({pageSize: 6})} active={this.state.pageSize === 6}>6</Button>
-                  <Button onClick={() => this.setState({pageSize: 9})} active={this.state.pageSize === 9}>9</Button>
-                </ButtonGroup>
-              </Col>
-            </Row>
-          </Col>
-          <Col sm='4'>
-            <Row>
-              <Col className='d-flex justify-content-center'>
-                Page Number Visibility Level
-              </Col>
-            </Row>
-            <Row>
-              <Col className='d-flex justify-content-center'>
-                <ButtonGroup>
-                  <Button onClick={() => this.setState({visibility: 0})} active={this.state.visibility === 0}>0</Button>
-                  <Button onClick={() => this.setState({visibility: 1})} active={this.state.visibility === 1}>1</Button>
-                  <Button onClick={() => this.setState({visibility: 2})} active={this.state.visibility === 2}>2</Button>
-                  <Button onClick={() => this.setState({visibility: 3})} active={this.state.visibility === 3}>3</Button>
-                </ButtonGroup>
-              </Col>
-            </Row>
-          </Col>
-          <Col sm='4'>
-            <Row>
-              <Col className='d-flex justify-content-center'>
-                Ellipsis Usage
-              </Col>
-            </Row>
-            <Row>
-              <Col className='d-flex justify-content-center'>
-                <ButtonGroup>
-                  <Button onClick={() => this.setState({ellipsis: true})} active={this.state.ellipsis}>Use ellipsis</Button>
-                  <Button onClick={() => this.setState({ellipsis: false})} active={!this.state.ellipsis}>No ellipsis</Button>
-                </ButtonGroup>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+        {this.renderControlsRow()}
         <Row>
           <Col className='d-flex justify-content-center'>
             Page {this.state.page}: Items from {this.state.fromItem} to {this.state.toItem} while total is {this.props.data.length}
@@ -149,22 +100,7 @@ class Sample2 extends React.Component {
         </Row>
         <Row>
           <Col className='d-flex justify-content-center'>
-            <Table>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Name</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.props.data.slice(this.state.fromItem, this.state.toItem + 1).map((d, i) =>
-                  <tr key={`row-${i}`}>
-                    <td>{this.state.fromItem + i}</td>
-                    <td>{d.name}</td>
-                  </tr>
-                )}
-              </tbody>
-            </Table>
+            {this.renderTable()}
           </Col>
         </Row>
         <Row>
@@ -183,6 +119,58 @@ class Sample2 extends React.Component {
         </Row>
       </Container>
     );
+  }
+
+  renderTable() {
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.data.slice(this.state.fromItem, this.state.toItem + 1).map((d, i) =>
+            <tr key={`row-${i}`}>
+              <td>{this.state.fromItem + i}</td>
+              <td>{d.name}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    )
+  }
+
+  renderControlsRow() {
+    return (
+      <Row>
+        <Col sm='4' className='d-flex justify-content-center'>
+          <ButtonGroup>
+            <Button disabled color='success'>Items in Page</Button>
+            <Button onClick={() => this.setState({pageSize: 3})} active={this.state.pageSize === 3}>3</Button>
+            <Button onClick={() => this.setState({pageSize: 6})} active={this.state.pageSize === 6}>6</Button>
+            <Button onClick={() => this.setState({pageSize: 9})} active={this.state.pageSize === 9}>9</Button>
+          </ButtonGroup>
+        </Col>
+        <Col sm='4' className='d-flex justify-content-center'>
+          <ButtonGroup>
+            <Button disabled color='warning'>Page Number Visibility Level</Button>
+            <Button onClick={() => this.setState({visibility: 0})} active={this.state.visibility === 0}>0</Button>
+            <Button onClick={() => this.setState({visibility: 1})} active={this.state.visibility === 1}>1</Button>
+            <Button onClick={() => this.setState({visibility: 2})} active={this.state.visibility === 2}>2</Button>
+            <Button onClick={() => this.setState({visibility: 3})} active={this.state.visibility === 3}>3</Button>
+          </ButtonGroup>
+        </Col>
+        <Col sm='4' className='d-flex justify-content-center'>
+            <ButtonGroup>
+              <Button disabled color='info'>Ellipsis Usage</Button>
+              <Button onClick={() => this.setState({ellipsis: true})} active={this.state.ellipsis}>Use</Button>
+              <Button onClick={() => this.setState({ellipsis: false})} active={!this.state.ellipsis}>Not use</Button>
+            </ButtonGroup>
+        </Col>
+      </Row>
+    )
   }
 }
 
